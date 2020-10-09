9a4b1f9a9d5edb3b5b95cad2ce3977e162e343d6
Zachary A. Cooper <60453155+zacoope1@users.noreply.github.com>
2020-10-08 18:28:03 -0700

    Create buildScript.sh
    Added buildscript

---
+++ buildScript.sh
@@ -0,0 +1,25 @@
+npm run build
+echo 'Build Finsihed.'
+
+(cd /mnt/dev1/capstone/docker && sudo docker-compose down)
+echo 'Bringing down apache server.'
+
+sudo rm -rf /mnt/dev1/capstone/docker/build_old
+echo 'removed build_old/.'
+
+sudo cp -r /mnt/dev1/capstone/docker/build /mnt/dev1/capstone/docker/build_old
+echo 'Copied previous build to build_old'
+
+sudo rm -rf /mnt/dev1/capstone/docker/build
+echo 'Removed previous build.'
+
+sudo cp -r build/ /mnt/dev1/capstone/docker
+echo 'Copied new build to /mnt/dev1/capstone/docker/build'
+
+(cd /mnt/dev1/capstone/docker && sudo chmod -R 777 build && sudo chmod -R 777 build_old )
+echo 'Setting file permissions'
+
+(cd /mnt/dev1/capstone/docker && sudo docker-compose up -d)
+echo 'Bringing up apache server.'
+
+echo 'Pipeline Finished!'
