bcf096fc243a6536c1309a4196735fedfa69ce93
r-tuck <redbogey98@gmail.com>
2020-10-09 15:22:01 -0700

    Images and CSS Files added/Main Menu Changed

--- src/components/SceneSelector.js
+++ src/components/SceneSelector.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {MENU_MODE, SATELLITE_MODE, ASTEROID_MODE} from '../constants/constants';
 import Satellite from './Satellite/Satellite'
 import Asteriod from './Asteroid/Asteriod'
+import '../constants/Menu.css'
+import Badge from '../assets/images/Psyche-Badge-Mono.png'
 
 
 class SceneSelector extends React.Component{
@@ -29,9 +31,20 @@ class SceneSelector extends React.Component{
     menuMode() {
         return (
             <div>
-                <h1>Select Your WebXR Experience!</h1>
-                <button onClick={ () => { this.setState({mode: ASTEROID_MODE}) } }>Asteroid Experience</button>
-                <button onClick={ () => { this.setState({mode: SATELLITE_MODE}) } }>Satellite Experience</button>
+                <div style = {{paddingBottom: '2vh', display: 'flex', justifyContent: 'center'}}>
+                    <h1>Select Your WebXR Experience!</h1>
+                </div>
+                <div style = {{padding: '2vh', display: 'flex', justifyContent: 'center'}}>
+                    <button onClick={ () => { this.setState({mode: ASTEROID_MODE}) } }>
+                        Asteroid Experience</button>
+                </div>
+                <div style = {{padding: '2vh', display: 'flex', justifyContent: 'center'}}>
+                    <button onClick={ () => { this.setState({mode: SATELLITE_MODE}) } }>
+                        Satellite Experience</button>
+                </div>
+                <div style = {{position: 'absolute', right: '5px', bottom: '5px'}}>
+                    <img className = "photo" src = {Badge} alt = "Psyche Badge"></img>
+                </div>
             </div>
         );
     }
